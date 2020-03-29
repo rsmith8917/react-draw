e2de4e56de5bfd0033eeaba87878fab45c8ecb6e
Robert Smith <rsmith8917@gmail.com>
2020-03-29 08:54:51 -0400

    animation loop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
 
 function App() {
+  const canvasRef = useRef(null);
+  const state = useRef({ objects: [] });
+
+  useEffect(function () {
+    window.requestAnimationFrame(draw);
+
+  }, []);
+
+
+  function draw() {
+    const ctx = canvasRef.current.getContext("2d");
+
+    // Clear the canvas
+    ctx.clearRect(0, 0, 640, 640);
+
+    // Color Background
+    ctx.fillStyle = "#dddddd";
+    ctx.fillRect(0, 0, 640, 425);
+
+    state.current.objects.forEach(o => {
+      ctx.fillStyle = o.fillStyle;
+      ctx.beginPath();
+      ctx.rect(o.x, o.y, o.width, o.height);
+      ctx.stroke();
+    });
+
+    window.requestAnimationFrame(draw);
+  }
+
+  function drawRectangle() {
+    state.current.objects.push({ type: 'Rectangle', selected: false, x: 20, y: 20, width: 150, height: 100, fillStyle: "#000000" });
+  }
+
+  function moveObject() {
+    const object = state.current.objects[0];
+    object.x += 20;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={drawRectangle}>Rectangle</button>
+      <button onClick={moveObject}>Move</button>
+      <br />
+      <canvas ref={canvasRef} width={640} height={425} />
     </div>
   );
 }
